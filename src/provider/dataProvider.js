5a6603703ef3bc04b222107a0c91c14090ea7c68
KaizJava_io <phunlhce150297@fpt.edu.vn>
2022-06-02 01:15:29 +0700

    v3

--- src/provider/dataProvider.js
+++ src/provider/dataProvider.js
@@ -13,8 +13,7 @@ const dataProvider = {
       range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
       filter: JSON.stringify(params.filter),
     };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
-
+    const url = `${apiUrl}/${resource}?${JSON.stringify(query)}`;
     return httpClient(url).then(({ headers, json }) => ({
       data: json.map((resource) => ({ ...resource, id: resource._id })),
       total: parseInt(headers.get("content-range").split("/").pop(), 10),
