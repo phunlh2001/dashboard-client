333bfdb3f74d59966848ff29d9afd10288cccfcb
KaizJava_io <phunlhce150297@fpt.edu.vn>
2022-06-04 14:42:07 +0700

    v11

--- src/provider/dataProvider.js
+++ src/provider/dataProvider.js
@@ -1,5 +1,5 @@
 import { fetchUtils } from "react-admin";
-import { stringify } from "query-string";
+// import { stringify } from "query-string";
 
 // const apiUrl = "http://localhost:3000";
 const apiUrl = "https://dashboard-account-review.netlify.app";
@@ -14,12 +14,12 @@ const dataProvider = {
       range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
       filter: JSON.stringify(params.filter),
     };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
+    const url = `${apiUrl}/${resource}?${query}`;
 
     return httpClient(url).then(({ headers, json }) => ({
       data: json.map((resource) => ({ ...resource, id: resource._id })),
-      total: parseInt(headers.get("content-range")?.split("/").pop(), 10),
       headers: new Headers({ Accept: "application/json" }),
+      total: parseInt(headers.get("content-range")?.split("/").pop(), 10),
     }));
   },
   getOne: (resource, params) =>
@@ -31,7 +31,7 @@ const dataProvider = {
     const query = {
       filter: JSON.stringify({ id: params.ids }),
     };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
+    const url = `${apiUrl}/${resource}?${query}`;
     return httpClient(url).then(({ json }) => ({
       data: json.map((resource) => ({ ...resource, id: resource._id })),
     }));
@@ -48,7 +48,7 @@ const dataProvider = {
         [params.target]: params.id,
       }),
     };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
+    const url = `${apiUrl}/${resource}?${query}`;
 
     return httpClient(url).then(({ headers, json }) => ({
       data: json.map((resource) => ({ ...resource, id: resource._id })),
@@ -66,7 +66,7 @@ const dataProvider = {
     const query = {
       filter: JSON.stringify({ id: params.ids }),
     };
-    return httpClient(`${apiUrl}/${resource}/update?${stringify(query)}`, {
+    return httpClient(`${apiUrl}/${resource}/update?${query}`, {
       method: "PUT",
       body: JSON.stringify(params.data),
     }).then(({ json }) => ({ data: json }));
@@ -93,13 +93,10 @@ const dataProvider = {
     const query = {
       filter: JSON.stringify({ id: params.id }),
     };
-    return httpClient(
-      `${apiUrl}/${resource}/delete?${JSON.stringify(stringify(query))}`,
-      {
-        method: "DELETE",
-        body: JSON.stringify(params.data),
-      }
-    ).then((responses) => ({
+    return httpClient(`${apiUrl}/${resource}/delete?${JSON.stringify(query)}`, {
+      method: "DELETE",
+      body: JSON.stringify(params.data),
+    }).then((responses) => ({
       data: responses.map(({ json }) => ({ ...json, id: json.id })),
     }));
   },
